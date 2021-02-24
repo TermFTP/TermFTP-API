2215e24cd4a76007743d36d312a7747514afa57a
TheCrether <thecrether@gmail.com>
2021-02-24 22:28:25 +0100

    :bug: fixed null-byte problem with PostgreSQL

--- src/main/java/at/termftp/backend/service/UserService.java
+++ src/main/java/at/termftp/backend/service/UserService.java
@@ -31,22 +31,24 @@ public class UserService {
 
     /**
      * used to get a single user by and ID
+     *
      * @param userID the User's ID
      * @return the user or null
      */
-    public User getUserById(UUID userID){
+    public User getUserById(UUID userID) {
         return userRepository.findById(userID).orElse(null);
     }
 
     /**
      * @return a list of all users
      */
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     /**
      * used to Hash a password using the SHA-256 algorithm (HASHING-DEPTH -times)
+     *
      * @param password the password to be hashed
      * @return the hash
      */
@@ -65,11 +67,12 @@ public class UserService {
             bytes = digest.digest(bytes); // hashing
         }
 
-        return new String(bytes, StandardCharsets.UTF_8);
+        return new String(bytes, StandardCharsets.UTF_8).replace(new String(new byte[]{0x00}, StandardCharsets.UTF_8), "");
     }
 
     /**
      * used to create a user
+     *
      * @return the user
      */
     public User createUser(User user) {
@@ -83,18 +86,20 @@ public class UserService {
 
     /**
      * used to get a user by username
+     *
      * @return the user or null
      */
-    public User getUserByName(String username){
+    public User getUserByName(String username) {
         return userRepository.findUserByUsername(username).orElse(null);
     }
 
     /**
      * deletes an user by userID
      * ------------ may throw exception when other tables still refer to this user -> delete from other tables first ------------------s
+     *
      * @return the number of deleted users
      */
-    public int deleteUser(User user){
+    public int deleteUser(User user) {
         int cToken = confirmationTokenRepository.deleteByUser(user.getUserID());
         System.out.println("-- Deleted " + cToken + " confirmationToken for user: " + user);
         int aToken = accessTokenRepository.deleteByUserID(user.getUserID());
